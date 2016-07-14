d68b9fe05159993e24dcc277c7b9ca78df15c493
platy11 <mightymaxmax@gmail.com>
2016-07-14 20:18:28 +1000

    Add mathematical constants

--- exponent.js
+++ exponent.js
@@ -1,7 +1,22 @@
-function(){
-  window.Exponent = function(){
-    var exponent;
-    Exponent = {}
-    return Exponent;
-  }();
-}();
+var Exponent = (function() {
+	var Exponent;
+	
+	Exponent = {};
+	
+	////////////
+	
+	// CONSTANTS
+	
+	Exponent.constants = {};
+	
+	Exponent.constants.Pi            = 3.14159265358979323846264338327950288419716939937510;
+	Exponent.constants.Phi           = 1.61803398874989484820458683436563811772030917980576;
+	Exponent.constants.root2         = 2.71828182845904523536028747135266249775724709369995;
+	Exponent.constants.root3         = 1.73205080756887729352744634150587236694280525381038;
+	Exponent.constants.root5         = 2.23606797749978969640917366873127623544061835961152;
+	Exponent.constants.eulerConstant = 0.57721566490153286060651209008240243104215933593992;
+	
+	////////////
+	
+	return Exponent;
+})();
